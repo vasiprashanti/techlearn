c413d4a9b6c5280bf826fcdf78ca4cc5923e2b7f
ohsoKool <rishikeshgunda9@gmail.com>
2025-07-31 18:58:49 +0530

    Commented function in userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   registerUser,
   loginUser,
-  updateUserProfile, // ✅ add this
+  // updateUserProfile,
 } from "../controllers/userController.js";
 
 const userRoutes = express.Router();
@@ -11,6 +11,6 @@ userRoutes.post("/register", registerUser);
 userRoutes.post("/login", loginUser);
 
 // ✅ Add the PUT route for updating avatar or other fields
-userRoutes.put("/user/:id", updateUserProfile);
+// userRoutes.put("/user/:id", updateUserProfile);
 
 export default userRoutes;
