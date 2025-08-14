e5d7c760422923d787a92984fb4584f9ead83174
Ahmad Ullah <114727856+Ahmad264@users.noreply.github.com>
2025-08-15 00:13:01 +0530

    commented out route for Upload Topics

--- frontend/src/components/AdminDashbaord/Admin_Sidebar.jsx
+++ frontend/src/components/AdminDashbaord/Admin_Sidebar.jsx
@@ -14,7 +14,7 @@ const menu = [
   { name: "Dashboard", icon: HiOutlineHome, path: "/admin" },
   { name: "Courses", icon: HiOutlineBookOpen, path: "/admin/courses" },
   { name: "Exercises", icon: HiOutlineChartBar, path: "/admin/upload-exercises" },
-  { name: "Upload Files", icon: HiOutlineUpload, path: "/admin/upload-topics" },
+  //{ name: "Upload Files", icon: HiOutlineUpload, path: "/admin/upload-topics" },
   {name : "Quizzes", icon: MdQuiz, path: "/admin/quizzes-upload" }
 ];
 
